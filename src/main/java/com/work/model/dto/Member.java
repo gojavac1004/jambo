73407544e9a1953f3703fbc0a008207681e948fc
USER <khim98@empal.com>
2021-04-24 22:05:46 +0900

    init welcome page, Member property

---
+++ src/main/java/com/work/model/dto/Member.java
@@ -0,0 +1,13 @@
+package com.work.model.dto;
+
+import java.io.Serializable;
+
+public class Member implements Serializable {
+	private String memberId;
+	private String memberPw;
+	private String name;
+	
+	public Member() {}
+	
+	
+}
