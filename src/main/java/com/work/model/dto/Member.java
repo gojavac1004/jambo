a90c1b3d9165d7b79d84080ffbd674a68c878d86
USER <USER@DESKTOP-28G81BL>
2021-04-24 22:10:44 +0900

    Member ovloading constructor

--- src/main/java/com/work/model/dto/Member.java
+++ src/main/java/com/work/model/dto/Member.java
@@ -8,6 +8,14 @@ public class Member implements Serializable {
 	private String name;
 	
 	public Member() {}
+
+	public Member(String memberId, String memberPw, String name) {
+		super();
+		this.memberId = memberId;
+		this.memberPw = memberPw;
+		this.name = name;
+	}
+	
 	
 	
 }
